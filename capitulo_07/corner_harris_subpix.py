a23219b061a541bc0e82a4c9df22c08ca8977984
Alex Bertolla <alex.bertolla@gmail.com>
2020-01-22 11:21:23 -0300

    Adição do diretório com imagens de lagartas Script harris_corner_detector.py e harris_corner_subpix.py

--- capitulo_07/corner_harris_subpix.py
+++ capitulo_07/corner_harris_subpix.py
@@ -20,13 +20,26 @@ from skimage.exposure import rescale_intensity
 from skimage.measure import ransac
 from skimage.color import rgb2gray
 
-image = imread('../images/chess.jpg') #RGB IMAGE
 
+image = imread('../images/pyramids2.jpg') #RGB IMAGE
 image_gray = rgb2gray(image)
 coordinates = corner_harris(image_gray, k=0.001)
-image[coordinates > 0.01 * coordinates.max()] = [255, 0, 255]
-pylab.figure(figsize=(20, 20))
-pylab.imshow(image), pylab.axis('off'), pylab.show()
 
+coordinates[coordinates > 0.03 * coordinates.max()] = 255 # THRESHOLD FOR AN OPTIMAL VALUE, DEPENDS ON THE IMAGE
+corner_coordinates = corner_peaks(coordinates)
+coordinates_subpix = corner_subpix(image_gray, corner_coordinates, window_size=11)
+pylab.figure(figsize=(20, 20))
+
+pylab.subplot(211), pylab.imshow(coordinates, cmap='inferno')
+pylab.plot(coordinates_subpix[:, 1], coordinates_subpix[:, 0], 'r', markersize=5, label='Subpixel')
+pylab.legend(prop={'size': 5})
+pylab.axis('off')
+
+pylab.subplot(212), pylab.imshow(image, interpolation='nearest')
+pylab.plot(corner_coordinates[:, 1], corner_coordinates[:, 0], 'bo', markersize=5)
+pylab.plot(coordinates_subpix[:, 1], coordinates_subpix[:, 0], 'r+', markersize=10)
+pylab.axis('off')
+pylab.tight_layout()
+pylab.show()
 
 print('FIM')
