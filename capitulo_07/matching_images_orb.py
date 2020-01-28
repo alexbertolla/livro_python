14feb69be547d29d31863c8e9e010843fee5a11e
Alex Bertolla <alex.bertolla@gmail.com>
2020-01-28 14:47:03 -0300

    Script matching_images_orb.py

--- capitulo_07/matching_images_orb.py
+++ capitulo_07/matching_images_orb.py
@@ -16,6 +16,48 @@ from scipy import misc
 from skimage import transform
 from skimage.feature import (match_descriptors, ORB, plot_matches)
 
+img1 = color.rgb2gray(imread('../images/lagarta/imagem_8_seg.jpg'))
+img2 = transform.rotate(img1, 180)
+
+affine_trans = transform.AffineTransform(scale=(1.3, 1.1), rotation=0.5, translation=(0, -200))
+
+img3 = transform.warp(img1, affine_trans)
+img4 = transform.resize(color.rgb2gray(imread('../images/lagarta/imagem_8.jpg')), img1.shape, anti_aliasing=True)
+
+descriptor_extractor = ORB(n_keypoints=200)
+descriptor_extractor.detect_and_extract(img1)
+keypoints1, descriptors1 = descriptor_extractor.keypoints, descriptor_extractor.descriptors
+
+
+descriptor_extractor.detect_and_extract(img2)
+keypoints2, descriptors2 = descriptor_extractor.keypoints, descriptor_extractor.descriptors
+
+
+descriptor_extractor.detect_and_extract(img3)
+keypoints3, descriptors3 = descriptor_extractor.keypoints, descriptor_extractor.descriptors
+
+descriptor_extractor.detect_and_extract(img4)
+keypoints4, descriptors4 = descriptor_extractor.keypoints, descriptor_extractor.descriptors
+
+matches12 = match_descriptors(descriptors1, descriptors2, cross_check=True)
+matches13 = match_descriptors(descriptors1, descriptors3, cross_check=True)
+matches14 = match_descriptors(descriptors1, descriptors4, cross_check=True)
+
+
+fig, axes = pylab.subplots(nrows=3, ncols=1, figsize=(20, 30))
+pylab.gray()
+
+plot_matches(axes[0], img1, img2, keypoints1, keypoints2, matches12)
+axes[0].axis('off'), axes[0].set_title('Original Image vs. Transformed Image')
+
+plot_matches(axes[1], img1, img3, keypoints1, keypoints3, matches13)
+axes[1].axis('off'), axes[1].set_title('Original Image vs. Transformed Image')
+
+plot_matches(axes[2], img1, img4, keypoints1, keypoints4, matches14)
+axes[2].axis('off'), axes[2].set_title('Original Image vs. Transformed Image')
+
+
+pylab.show()
 
 
 print('FIM')
