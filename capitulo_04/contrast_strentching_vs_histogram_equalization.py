d93c7cbf7d12cc88836f75af2891464817d13415
alex <alex.bertolla@gmail.com>
2020-04-14 16:00:37 -0300

    contrast_strentching_vs_histogram_equalization

--- capitulo_04/contrast_strentching_vs_histogram_equalization.py
+++ capitulo_04/contrast_strentching_vs_histogram_equalization.py
@@ -9,8 +9,63 @@ from skimage.color import rgb2gray
 from PIL import Image, ImageEnhance, ImageFilter
 from scipy import ndimage, misc
 import matplotlib.pylab as pylab
+import matplotlib
 
+matplotlib.rcParams['font.size'] = 8
 
+def plot_image_and_hist(image, axes, bins=256):
+    image = img_as_float(image)
+    axes_image, axes_hist = axes
+    axes_cdf = axes_hist.twinx()
+    axes_image.imshow(image, cmap=pylab.cm.gray)
+    axes_image.set_axis_off()
+    axes_hist.hist(image.ravel(), bins=bins, histtype='step', color='black')
+    axes_hist.set_xlim(0, 1)
+    axes_hist.set_xlabel('Pixel intensity', size=15)
+    axes_hist.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
+    axes_hist.set_yticks([])
+    image_cdf, bins = exposure.cumulative_distribution(image, bins)
+    axes_cdf.plot(bins, image_cdf, 'r')
+    axes_cdf.set_yticks([])
+    return axes_image, axes_hist, axes_cdf
+
+im = io.imread('../images/beans_g.png')
+#im = rgb2gray(im)
+p2, p98 = np.percentile(im, (2, 98)) #contrast strentching
+im_rescale = exposure.rescale_intensity(im, in_range=(p2, p98))
+im_eq = exposure.equalize_hist(im) #histogram equalization
+im_adapteq = exposure.equalize_adapthist(im, clip_limit=0.03) #adaptative histogram equalization
+
+fig = pylab.figure(figsize=(15, 7))
+axes = np.zeros((2, 4), dtype=np.object)
+axes[0, 0] = fig.add_subplot(2, 4, 1)
+
+for i in range(1, 4):
+    axes[0, i] = fig.add_subplot(2, 4, 1+i, sharex=axes[0, 0], sharey=axes[0, 0])
+
+for i in range(0, 4):
+    axes[1, i] = fig.add_subplot(2, 4, 5+i)
+
+axes_image, axes_hist, axes_cdf = plot_image_and_hist(im, axes[:, 0])
+axes_image.set_title('Low contrast image', size=20)
+
+y_min, y_max = axes_hist.get_ylim()
+axes_hist.set_ylabel('Number of pixels', size=20)
+axes_hist.set_yticks(np.linspace(0, y_max, 5))
+
+axes_image, axes_hist, axes_cdf = plot_image_and_hist(im_rescale, axes[:, 1])
+axes_image.set_title('Contrast strentching', size=20)
+
+axes_image, axes_hist, axes_cdf = plot_image_and_hist(im_eq, axes[:, 2])
+axes_image.set_title('Histogram equalization', size=20)
+
+axes_image, axes_hist, axes_cdf = plot_image_and_hist(im_adapteq, axes[:, 3])
+axes_image.set_title('Adaptative equalization', size=20)
+
+axes_cdf.set_ylabel('Fraction of total intensity', size=20)
+axes_cdf.set_yticks(np.linspace(0, 1, 5))
+fig.tight_layout()
+pylab.show()
 
 
 print('FIM')
