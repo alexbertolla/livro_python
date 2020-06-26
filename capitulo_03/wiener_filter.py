94d3b108b20f087257dc128403be65b08efb56ff
Alex Bertolla <alex.bertolla@gmail.com>
2020-06-25 21:05:28 -0300

    non-local means filter

--- capitulo_03/wiener_filter.py
+++ capitulo_03/wiener_filter.py
@@ -1,3 +1,25 @@
+from skimage import color, data, restoration, filters
+from skimage.io import imread
+from scipy.signal import convolve2d as conv2
+import numpy as np
+from matplotlib import pylab
 
+im_o = imread('../images/69015.jpg')
+#im_o = color.rgb2gray(im_o)
+
+im_g = filters.gaussian(im_o, sigma=5)
+
+n = 4
+psf = np.ones((n, n)) / n**2
+im_w, _ = restoration.unsupervised_wiener(color.rgb2gray(im_g), psf)
+
+fig, axes = pylab.subplots(nrows=1, ncols=3, figsize=(5, 5), sharex=True, sharey=True)
+pylab.gray()
+
+axes[0].imshow(im_o), axes[0].axis('off'), axes[0].set_title('Origianl Images', size=20)
+axes[1].imshow(im_g), axes[1].axis('off'), axes[1].set_title('Gaussian Blurred Image', size=20)
+#axes[2].imshow(im_w), axes[2].axis('off'), axes[2].set_title('Wiener Filtered Image', size=20)
+
+pylab.show()
 
 print('FIM')
--- capitulo_04/skimage_non_local_means_filter.py
+++ capitulo_04/skimage_non_local_means_filter.py
@@ -4,6 +4,7 @@ from skimage.io import imread
 from skimage.exposure import cumulative_distribution
 from skimage.restoration import denoise_bilateral, denoise_nl_means, estimate_sigma
 from skimage.measure import compare_psnr
+from skimage.metrics import peak_signal_noise_ratio
 from skimage.util import random_noise
 from skimage.color import rgb2gray
 from PIL import Image, ImageEnhance, ImageFilter
@@ -33,6 +34,29 @@ print('estimated noise standard deviation = {}'.format(sigma_est))
 patch_wk = dict(patch_size=5, patch_distance=6, multichannel=True)
 
 #slow algorithm
+denoise = denoise_nl_means(noisy, h=1.15 * sigma_est, fast_mode=False, **patch_wk)
 
+#fat algorithm
+denoise_fast = denoise_nl_means(noisy, h=0.8 * sigma_est, fast_mode=True, **patch_wk)
+
+fig, axes = pylab.subplots(nrows=2, ncols=2, figsize=(15, 12), sharex=True, sharey=True)
+plot_image_axes(noisy, axes[0, 0], 'noisy')
+plot_image_axes(denoise, axes[0, 1], 'non-local means (slow)')
+plot_image_axes(parrot, axes[1, 0], 'original (noise free)')
+plot_image_axes(denoise_fast, axes[1, 1], 'non-local means (fast)')
+
+#PSNR metrics values
+#UserWarning: DEPRECATED: skimage.measure.compare_psnr has been moved to skimage.metrics.peak_signal_noise_ratio. It will be removed from skimage.measure in version 0.18.
+psnr_noisy = peak_signal_noise_ratio(parrot, noisy)
+psnr = peak_signal_noise_ratio(parrot, denoise.astype(np.float64))
+psnr_fast = peak_signal_noise_ratio(parrot, denoise_fast.astype(np.float64))
+
+print('PSNR (noisy) = {:0.2f}'. format((psnr_noisy)))
+print('PSNR (slow) = {:0.2f}'. format((psnr)))
+print('PSNR (fast) = {:0.2f}'. format((psnr_fast)))
+
+
+
+pylab.show()
 
 print('FIM')
