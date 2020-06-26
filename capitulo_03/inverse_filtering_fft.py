bcf54e57ba754afd3361f9dcf23637e2bf29a681
Alex Bertolla <alex.bertolla@gmail.com>
2020-06-25 22:30:56 -0300

    inverse_filtering_fft.py deblur

--- capitulo_03/inverse_filtering_fft.py
+++ capitulo_03/inverse_filtering_fft.py
@@ -3,6 +3,7 @@ import numpy.fft as fp
 from skimage.io import imread
 from skimage.color import rgb2gray
 from skimage import img_as_float, img_as_uint, img_as_ubyte
+from skimage.filters import gaussian
 from scipy import signal
 from matplotlib import pylab
 import cv2 as cv
@@ -25,18 +26,25 @@ im_restored = fp.ifft2(convolved).real
 im_restored = 255 * im_restored / np.max(im_restored)
 
 ########################################################################################################################
-im_cv = cv.cvtColor(cv.imread('../images/lena.jpg'), cv.COLOR_BGR2GRAY)
-im_cv_b = cv.blur(im_cv, (5, 5))
-im_cv_b = img_as_float(im_cv_b)
+
+im2 = rgb2gray(imread('../images/lena.jpg'))
+im2 = gaussian(im2, 5)
+gauss_kernel2 = np.outer(signal.gaussian(im2.shape[0], 3), signal.gaussian(im2.shape[1], 3))
+print(gauss_kernel2)
+
+freq2 = fp.fft2(im2)
+freq_kernel2 = fp.fft2(fp.ifftshift(im2)) #this is our H
+convolved2 = freq2*freq_kernel2 #by convolution theorem
+im_blur2 = fp.ifft2(convolved2).real
+im_blur2 = 255 * im_blur2 / np.max(im_blur2) #normalize
 
 epsilon = 10**-6
-freq = fp.fft2(im_cv_b)
-freq_kernel = 1 / (epsilon + freq_kernel) #avoid division by zero
-convolved = freq*freq_kernel
+freq2 = fp.fft2(im_blur2)
+freq_kernel2 = 1 / (epsilon + freq_kernel2) #avoid division by zero
+convolved2 = freq*freq_kernel2
 
-im_cv_r = fp.ifft2(convolved).real
-im_cv_r = 255 * im_cv_r / np.max(im_cv)
-
+im_restored2 = fp.ifft2(convolved).real
+im_restored2 = 255 * im_restored2 / np.max(im_restored2)
 
 
 
@@ -46,8 +54,8 @@ pylab.figure(figsize=(5, 5))
 pylab.subplot(321), pylab.imshow(im, cmap='gray'), pylab.title('Original Image'), pylab.axis('off')
 pylab.subplot(322), pylab.imshow(im_blur, cmap='gray'), pylab.title('Blurred Image'), pylab.axis('off')
 pylab.subplot(323), pylab.imshow(im_restored, cmap='gray'), pylab.title('Restored Image'), pylab.axis('off')
-pylab.subplot(324), pylab.imshow(im_cv_b, cmap='gray'), pylab.title('OPEN CV Image Blurred'), pylab.axis('off')
-pylab.subplot(325), pylab.imshow(im_cv_r, cmap='gray'), pylab.title('OPEN CV Image Restored'), pylab.axis('off')
+pylab.subplot(325), pylab.imshow(im2, cmap='gray'), pylab.title('Blurred Image 2'), pylab.axis('off')
+pylab.subplot(326), pylab.imshow(im_restored2, cmap='gray'), pylab.title('Restored Image 2'), pylab.axis('off')
 
 pylab.show()
 print('FIM')
